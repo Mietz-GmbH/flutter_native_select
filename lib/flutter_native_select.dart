b8776c24fcbb32fc14cb6bb4c88297722ca893f5
Maximilian Schelbach <6402841+maximilianschelbach@users.noreply.github.com>
2021-07-01 23:40:15 +0200

    implemented platform interface

--- lib/flutter_native_select.dart
+++ lib/flutter_native_select.dart
@@ -1,14 +1,56 @@
-
 import 'dart:async';
+import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/services.dart';
+
+class NativeSelectItem {
+  final String value;
+  final String label;
+  final bool disabled;
+  final Color? color;
+
+  const NativeSelectItem({
+    required this.value,
+    required this.label,
+    this.disabled = false,
+    this.color,
+  });
+}
 
 class FlutterNativeSelect {
   static const MethodChannel _channel =
       const MethodChannel('flutter_native_select');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String?> openSelect({
+    required List<NativeSelectItem> items,
+    String? defaultValue,
+    String doneText = 'Done',
+    String? clearText,
+  }) {
+    assert(items.map((e) => e.value).toSet().length == items.length,
+        'Values in select items must be unique!');
+    assert(
+        defaultValue == null ||
+            items.any((element) => element.value == defaultValue),
+        'Default value must be one of the given items!');
+
+    return _channel.invokeMethod(
+        'openSelect',
+        jsonEncode({
+          'items': [
+            for (final item in items)
+              {
+                'value': item.value,
+                'label': item.label,
+                'disabled': item.disabled,
+                'color': item.color?.value,
+              }
+          ],
+          'defaultValue': defaultValue ??
+              items.firstWhere((element) => !element.disabled).value,
+          'doneText': doneText,
+          'clearText': clearText,
+        }));
   }
 }
