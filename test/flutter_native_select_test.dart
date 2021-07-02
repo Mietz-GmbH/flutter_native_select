cfc3af8b7f9a33eb2045fd8f8d93b80b17346072
Maximilian Schelbach <6402841+maximilianschelbach@users.noreply.github.com>
2021-07-02 16:59:43 +0200

    implemented some tests

--- test/flutter_native_select_test.dart
+++ test/flutter_native_select_test.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:ui';
+
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_native_select/flutter_native_select.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const color1 = Color(0xff000000);
+const color2 = Color(0xff0000ff);
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_native_select');
@@ -8,16 +14,84 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    channel.setMockMethodCallHandler(
+        (methodCall) => Future.value(methodCall.arguments));
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterNativeSelect.platformVersion, '42');
+  test('check for duplicated items', () {
+    expect(
+        () => FlutterNativeSelect.openSelect(items: [
+              NativeSelectItem(value: 'item', label: ''),
+              NativeSelectItem(value: 'item', label: '')
+            ]),
+        throwsAssertionError);
+  });
+
+  test('check that default is is an actual item', () {
+    expect(
+        () => FlutterNativeSelect.openSelect(
+              items: [
+                NativeSelectItem(value: 'item', label: ''),
+              ],
+              defaultValue: 'missing',
+            ),
+        throwsAssertionError);
+  });
+
+  test('check that data are correctly transferred', () async {
+    final value = await FlutterNativeSelect.openSelect(
+      items: [
+        NativeSelectItem(
+          value: 'item1',
+          label: 'Item 1',
+          disabled: true,
+          color: color1,
+        ),
+        NativeSelectItem(
+          value: 'item2',
+          label: 'Item 2',
+          color: color2,
+        ),
+        NativeSelectItem(
+          value: 'item3',
+          label: 'Item 3',
+        ),
+      ],
+      defaultValue: 'item3',
+      doneText: 'Done text',
+      clearText: 'Clear text',
+      title: 'Title',
+    );
+    final decodedValue = jsonDecode(value!);
+    expect(decodedValue, {
+      'items': [
+        {
+          'value': 'item1',
+          'label': 'Item 1',
+          'disabled': true,
+          'color': color1.value,
+        },
+        {
+          'value': 'item2',
+          'label': 'Item 2',
+          'disabled': false,
+          'color': color2.value,
+        },
+        {
+          'value': 'item3',
+          'label': 'Item 3',
+          'disabled': false,
+          'color': null,
+        },
+      ],
+      'defaultValue': 'item3',
+      'doneText': 'Done text',
+      'clearText': 'Clear text',
+      'title': 'Title',
+    });
   });
 }
